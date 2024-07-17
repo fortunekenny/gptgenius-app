2dcab24c879443076ccf3425c2be1f7232701f58
fortunekenny <gbephraim@gmail.com>
2024-07-17 23:39:03 +0100

    sidebar setup

--- app/components/MemberProfile.jsx
+++ app/components/MemberProfile.jsx
@@ -1,4 +1,21 @@
-const MemberProfile = () => {
-  return <div>MemberProfile</div>;
+import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+
+const MemberProfile = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="px-4 flex items-center gap-2">
+      <UserButton afterSignOutUrl="/" />
+      {user.emailAddresses && user.emailAddresses.length > 0 && (
+        <p>{user.emailAddresses[0].emailAddress}</p>
+      )}
+    </div>
+  );
 };
+
 export default MemberProfile;
